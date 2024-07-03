f5c4c972a7b7b4835cdf1c4be044a3f005dd613b
vinay1g-commits <vinayuworkTest12@gmail.com>
2024-07-03 16:18:56 +0530

    first commit

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,8 @@ import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+
 from utilities import configreader
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -14,10 +16,14 @@ def before_scenario(context, scenario):
     browser_name = configreader.reading_data_ini("basic info", "browser")
 
     if browser_name == "chrome":
-        options = ChromeOptions()
-        chromedriver_path = "/usr/local/bin/chromedriver"
-        service = ChromeService(executable_path=chromedriver_path)
-        context.driver = webdriver.Chrome(service=service, options=options)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        #options.add_argument('--headless')  # if you're running in a headless environment
+        options.add_argument('--disable-gpu')
+        options.add_argument('--remote-debugging-port=9222')
+
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
     elif browser_name == "firefox":
         firefox_binary_path = "/usr/bin/firefox"  # Verify this path using `which firefox`
