69815b0aa39c3b14750f1c4e5f88349ec554ac6f
vinay1g-commits <vinayuworkTest12@gmail.com>
2024-07-03 16:39:37 +0530

    first commit

--- features/environment.py
+++ features/environment.py
@@ -17,7 +17,7 @@ def before_scenario(context, scenario):
 
     if browser_name == "chrome":
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
