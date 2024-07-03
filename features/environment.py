3cc70433d7c2027bd274a51a0d98dc1f0b462db9
vinay1g-commits <vinayuworkTest12@gmail.com>
2024-07-03 16:35:18 +0530

    first commit

--- features/environment.py
+++ features/environment.py
@@ -20,6 +20,7 @@ def before_scenario(context, scenario):
         #chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--remote-debugging-port=9222')
 
         # Ensure you do not pass options multiple times
